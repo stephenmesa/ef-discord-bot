4995e6e4b2ac5c7a13c35d0fc27542c49c752565
Stephen Mesa <stephen.mesa@gmail.com>
2018-07-19 20:45:50 -0700

    prettier message!

--- index.js
+++ index.js
@@ -22,12 +22,35 @@ function calculate(msg, totalStr, rateStr, efficiencyStr) {
   var percentage = (medalsGained / total) * 100;
   var percentageWithDoubled = ((medalsGained * 2) / total) * 100;
 
-  var messageToSend = 'User: ' + msg.author + '. Your SR rate percentage is ' +
-    percentage.toFixed(2).toString() + '% (' + utils.formatGoldString(medalsGained) +
-    ') without doubling and ' +
-    percentageWithDoubled.toFixed(2).toString() + '% (' + utils.formatGoldString(medalsGained * 2) +
-    ') with doubling.';
-  msg.channel.send(messageToSend);
+  msg.channel.send({
+    embed:
+    {
+        author: {
+            name: msg.author.name,
+            icon_url: 'https://cdn.discordapp.com/avatars/' + msg.author.id + '/' + msg.author.avatar + '.png'
+        },
+        footer: {
+            icon_url: "https://cdn.discordapp.com/avatars/294466905073516554/dcde95b6bfc77a0a7eb62827fd87af1a.png",
+            text: "NephBot create by @stephenmesa#1219"
+        },
+        title: "Spirit Rest calulator",
+        description: 'User: ' + msg.author,
+        color: 13720519,
+        timestamp: new Date().toISOString(),
+        fields: [
+            {
+                name: "Spirit Rest",
+                value: percentage.toFixed(2).toString() + '% (' + utils.formatGoldString(medalsGained) + ')',
+                inline: true
+            },
+            {
+                name: "Spirit Rest Doubled",
+                value: percentageWithDoubled.toFixed(2).toString() + '% (' + utils.formatGoldString(medalsGained * 2) + ')',
+                inline: true
+            }
+        ]
+    }
+  });
 }
 
 client.on('ready', function() {
