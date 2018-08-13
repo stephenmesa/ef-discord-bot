27d761b74f93ad5f600361ce79e92981ad99a991
Sakura Duff <kryptokards@gmail.com>
2018-08-13 19:10:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import path from 'path';
 import Discord from 'discord.js';
 import _ from 'lodash';
 
-var Bot = {
-	prefix: '!',
-	owner: '294466905073516554',
-	Discord: Discord
+const Bot = {
+  prefix: '!',
+  owner: '294466905073516554',
+  Discord,
 };
 
 const client = new Discord.Client();
@@ -14,25 +14,15 @@ const cooldowns = new Discord.Collection();
 
 const discordToken = process.env.DISCORD_TOKEN;
 
-Bot.replyDelete = function(message, reply, deleteCommand = false)
-{
-	return message.reply(reply)
-	.then(msg => {
-		msg.delete(6000);
-		if(deleteCommand) message.delete();
-	});
+Bot.replyDelete = function replyDelete(message, reply, deleteCommand = false) {
+  return message.reply(reply)
+    .then((msg) => {
+      msg.delete(6000);
+      if (deleteCommand) message.delete();
+    });
 };
-Bot.FindUser = function(user, message)
-{
-	const mention = message.mentions.users.first();
-	if(mention) return mention;
-	else {
-		return message.guild.members.find(member => member.displayName.toLowerCase() === user.trim().toLowerCase());
-	}
-};
-Bot.sendMessage = function(user, message)
-{
-	client.fetchUser(user).send(message);
+Bot.sendMessage = function sendMessage(user, message) {
+  client.fetchUser(user).send(message);
 };
 
 if (!discordToken) {
@@ -44,63 +34,65 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', async message => {
+client.on('message', async (message) => {
   if (!message.content.startsWith(Bot.prefix) || message.author.bot) return;
 
-	const args = message.content.slice(Bot.prefix.length).trim().split(/ +/g);
-	const commandName = args.shift().toLowerCase();
+  const args = message.content.slice(Bot.prefix.length).trim().split(/ +/g);
+  const commandName = args.shift().toLowerCase();
 
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+  const command = client.commands.get(commandName)
+    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-	if (!command) return;
+  if (!command) return;
 
-	if (command.guildOnly && message.channel.type !== 'text') {
-		return message.reply('I can\'t execute that command inside DMs!');
-	}
-	if(message.author.id !== Bot.owner && ((command.role && !message.member.roles.has(command.role)) || command.permission && !message.member.hasPermission(command.permission)) )
-	{
-		return message.reply('You do not have access to this command!');
-	}
-	if (command.args && args.length < command.args) {
-		let reply = `You didn't provide enough arguments, ${message.author}!`;
+  if (command.guildOnly && message.channel.type !== 'text') {
+    message.reply('I can\'t execute that command inside DMs!');
+    return;
+  }
+  if (message.author.id !== Bot.owner
+    && (command.permission && !message.member.hasPermission(command.permission))) {
+    message.reply('You do not have access to this command!');
+    return;
+  }
+  if (command.args && args.length < command.args) {
+    let reply = `You didn't provide enough arguments, ${message.author}!`;
 
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${Bot.prefix}${command.name} ${command.usage}\``;
-		}
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${Bot.prefix}${command.name} ${command.usage}\``;
+    }
 
-		return message.channel.send(reply);
-	}
+    message.channel.send(reply);
+    return;
+  }
 
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
 
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
 
-	if (!timestamps.has(message.author.id)) {
-		timestamps.set(message.author.id, now);
-		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-	}
-	else {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+  if (!timestamps.has(message.author.id)) {
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+  } else {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		}
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+      return;
+    }
 
-		timestamps.set(message.author.id, now);
-		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-	}
-	try {
-		command.execute(message, args);
-	}
-	catch (error) {
-		message.reply('there was an error trying to execute that command!');
-	}
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+  }
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    message.reply('there was an error trying to execute that command!');
+  }
 });
 
 (() => {
@@ -113,17 +105,16 @@ client.on('message', async message => {
 
 Bot.client = client;
 module.exports = {
-	Bot: Bot
+  Bot,
 };
 
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const commands = require(`./commands/${file}`).commands;
-	_.each(commands, function(command)
-	{
-		client.commands.set(command.name, command);
-	});
-}
+_.each(commandFiles, (file) => {
+  const [commands] = require(`./commands/${file}`).commands; // eslint-disable-line global-require, import/no-dynamic-require
+  _.each(commands, (command) => {
+    client.commands.set(command.name, command);
+  });
+});
