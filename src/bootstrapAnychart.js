a2a88dddaee4f55a902a3a6d81df2d199baa56e2
stephenmesa <stephen.mesa@gmail.com>
2018-09-12 18:53:59 -0700

    RIP graphs (#16)
    * RIP graphs
    
    * better deprecation message

--- src/bootstrapAnychart.js
+++
@@ -1,9 +0,0 @@
-import makeAnychart from 'anychart';
-import makeAnychartNodejs from 'anychart-nodejs';
-import { JSDOM } from 'jsdom';
-
-const jsdom = new JSDOM('<body><div id="container"></div></body>', { runScripts: 'dangerously' });
-const { window } = jsdom;
-
-export const anychart = makeAnychart(window);
-export const anychartExport = makeAnychartNodejs(anychart);
--- src/commands/graph.js
+++ src/commands/graph.js
@@ -1,67 +1,9 @@
-import Discord from 'discord.js';
-import * as utils from '../utils';
-import * as datastore from '../datastore';
-import * as chart from '../chart';
-
-const sendGraphMessage = ({
-  message,
-  chartFilename,
-  chartType,
-}) => {
-  const re = new Discord.RichEmbed()
-    .setAuthor(message.member.displayName, message.author.avatar ? `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png` : undefined)
-    .setColor(13720519)
-    .setFooter('NephBot created by @stephenmesa#1219', 'https://cdn.discordapp.com/avatars/294466905073516554/dcde95b6bfc77a0a7eb62827fd87af1a.png')
-    .setImage('attachment://chart.jpg')
-    .attachFile(new Discord.Attachment(chartFilename, 'chart.jpg'))
-    .setDescription(`Here's your recent progress on ${chartType} gain!`);
-  message.channel.send(re).then(() => {
-    chart.deleteChart(chartFilename);
-  });
-};
-
 const graphCommand = {
   name: 'graph',
-  description: 'Graphs your progress',
-  usage: '[kl/medals] (blank for both)',
-  execute: (message, args) => {
-    datastore.getAllProgressEntries(message.author.id)
-      .then((results) => {
-        const dataPoints = results.map(p => ({
-          kl: Number(p.kl),
-          totalMedals: utils.parseGoldString(p.totalMedals),
-          srRate: p.percentage,
-          timestamp: p.timestamp,
-        }));
-
-        const graphType = args[0] ? args[0].toLowerCase() : args[0];
-
-        if (graphType === 'kl') {
-          chart.generateKlChart(dataPoints).then((chartFilename) => {
-            sendGraphMessage({
-              message,
-              chartFilename,
-              chartType: 'KL',
-            });
-          });
-        } else if (graphType === 'medals') {
-          chart.generateMedalsChart(dataPoints).then((chartFilename) => {
-            sendGraphMessage({
-              message,
-              chartFilename,
-              chartType: 'medal',
-            });
-          });
-        } else {
-          chart.generateKLAndMedalsChart(dataPoints).then((chartFilename) => {
-            sendGraphMessage({
-              message,
-              chartFilename,
-              chartType: 'KL and medal',
-            });
-          }).catch(console.error);
-        }
-      });
+  description: 'DEPRECATED',
+  usage: 'DEPRECATED',
+  execute: (message) => {
+    message.reply('That command has been removed for now, since it seems to bloat the bot and cause it to crash!');
   },
 };
 
