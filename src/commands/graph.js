3289a0acb7cde6e033bc6777d1eb446b8f815856
Stephen Mesa <stephen.mesa@gmail.com>
2018-09-15 09:44:49 -0700

    Include link to graph webapp in graph command output

--- src/commands/graph.js
+++ src/commands/graph.js
@@ -1,9 +1,10 @@
 const graphCommand = {
   name: 'graph',
-  description: 'DEPRECATED',
-  usage: 'DEPRECATED',
+  description: 'Work In Progress',
   execute: (message) => {
-    message.reply('That command has been removed for now, since it seems to bloat the bot and cause it to crash!');
+    const userId = message.author.id;
+    const graphUrl = `https://ef-discord-bot.appspot.com/graph/${userId}`;
+    message.reply(`That command has been removed for now, since it seems to bloat the bot and cause it to crash! Here's a beta version of the graphing site: ${graphUrl}`);
   },
 };
 
